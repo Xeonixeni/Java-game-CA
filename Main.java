83c211d5d1024982a2736b18e96bba9629e56878
Xeonixeni <a.c.prince@lancaster.ac.uk>
2017-05-10 22:08:03 +0100

    Added basic button listeners and implemented them.

--- Main.java
+++ Main.java
@@ -5,16 +5,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.ImageIcon;
+import java.util.Random;
 
-public class Main
+public class Main implements ActionListener
 {
     public JFrame window;				 
-  
 	public final int Rows = 13;
 	public final int Colums = 9;
 	public JButton Buttons[][] = new JButton[Rows][Colums];
 	public JPanel panel;
-	
+	private int s = 0;
+	public int Xloc = 0;
+	public int Yloc = 0;
+	public String[][] buttonIcons = new String [Rows][Colums];
         public void create()
         {
         window = new JFrame();
@@ -32,14 +35,17 @@ public class Main
 				if(i == 0)
 				{
 					Buttons[i][j] = new JButton(new ImageIcon("bank2.jpg"));
+					Buttons[i][j].addActionListener(this);
 				}
 				else if(i == 12)
 				{
 					Buttons[i][j] = new JButton(new ImageIcon("bank1.jpg"));
+					Buttons[i][j].addActionListener(this);
 				}
 				else
 				{
 					Buttons[i][j] = new JButton(new ImageIcon("water1.jpg"));
+					Buttons[i][j].addActionListener(this);
 				}					
 				Buttons[i][j].setPreferredSize(new Dimension(32, 32));
 			    Buttons[i][j].setMinimumSize(new Dimension(32, 32));
@@ -48,7 +54,7 @@ public class Main
 				Buttons[i][j].revalidate();
 				panel.add(Buttons[i][j]); 
 			}
-
+			
 		}
 			
 			
@@ -59,42 +65,93 @@ public class Main
 	public void level1()
 	{
 		panel.removeAll();
+		Random rand = new Random();
+		int n = rand.nextInt(11 - 8 + 1) + 8;
+		s = rand.nextInt(9);
+		int stumpLim = 0;		
 		for(int i=0;i<Rows;i++)
 		{
+			int q = rand.nextInt(9); 
 			for(int j = 0; j<Colums; j++)
 			{
-				
+				int stumpLoc1 = rand.nextInt(15);
+				int stumpLoc2 = rand.nextInt(15);
 				if(i == 0)
 				{
-					if(j == 2)
+					if(j == s)
 					{
 						Buttons[i][j] = new JButton(new ImageIcon("stump3.jpg"));
+						buttonIcons[i][j] = "stump3.jpg";
 					}
 					else
 					{
 						Buttons[i][j] = new JButton(new ImageIcon("bank2.jpg"));
+						buttonIcons[i][j] = "bank2.jpg";
 					}
 					
 				}
 				else if(i == 12)
 				{
-					if(j == 5)
+					if(j == s)
 					{
 						Buttons[i][j] = new JButton(new ImageIcon("stump2_man.jpg"));
+						Xloc = j;
+						Yloc = i;
+						buttonIcons[i][j] = "stump2_man.jpg";
 					}
 					else
 					{
 						Buttons[i][j] = new JButton(new ImageIcon("bank1.jpg"));
+						buttonIcons[i][j] = "bank1.jpg";
 					}
-					
 				}
-				else
-				{
+				else 
+				{	
+					if (i == n)
+					{
+						if(j == s)
+						{
+							Buttons[i][j] = new JButton(new ImageIcon("stump1.jpg"));
+							buttonIcons[i][j] = "stump1.jpg";
+						}
+				
+					}
+					else if( i > n)
+					{
+						if (j == s)
+						{
+							Buttons[i][j] = new JButton(new ImageIcon("Plank2.jpg"));
+							buttonIcons[i][j] = "Plank2.jpg";
+						}
+						
+					}
+					else
+					{
+						{
+						if( j == stumpLoc1)
+						{
+							Buttons[i][j] = new JButton(new ImageIcon("stump1.jpg"));
+							stumpLim++;
+							buttonIcons[i][j] = "stump1.jpg";
+						}
+						else if(j == stumpLoc2)	
+						{
+							Buttons[i][j] = new JButton(new ImageIcon("stump1.jpg"));
+							stumpLim++;
+							buttonIcons[i][j] = "stump1.jpg";
+						}
+						else 
+						{
+							Buttons[i][j] = new JButton(new ImageIcon("water1.jpg"));
+							buttonIcons[i][j] = "water1.jpg";
+						}
+						}
+					}
+				}
+			
 					
-
-					Buttons[i][j] = new JButton(new ImageIcon("water1.jpg"));
-					
-				}					
+				
+				Buttons[i][j].addActionListener(this);
 				Buttons[i][j].setPreferredSize(new Dimension(32, 32));
 			    Buttons[i][j].setMinimumSize(new Dimension(32, 32));
 				Buttons[i][j].setMaximumSize(new Dimension(32, 32));
@@ -107,7 +164,73 @@ public class Main
 		window.setContentPane(panel);
 		window.setLayout(new GridLayout(13, 9));
         window.pack();
+		System.out.println(Xloc + "  " + Yloc);
+		check();
+		
 	}
+	
+	public void check()
+	{
+		int limCheck = 12;
+		boolean stumpFound = false;
+		for(int j = 0; j<Colums; j++)
+		{
+		  String stumpCheck = Buttons[limCheck][j].getIcon().toString();
+			if(stumpCheck == "stump2_man.jpg")
+			{
+				stumpFound = true;
+			}
+			
+		}
+		if(stumpFound = false)
+		{
+			level1(); 
+		}
+		
+	}
+	public void actionPerformed(ActionEvent e) 
+	{
+		String btnType = "";
+		
+		
+		for(int i=0;i<Rows;i++)
+		{
+			for(int j = 0; j<Colums; j++)
+			{
+				if (e.getSource() == Buttons[i][j])
+				{
+					btnType = buttonIcons[i][j];	//Needs more work to be 100% - also to do with other areas
+					buttonIcons[i][j] = buttonIcons[Yloc][Xloc];
+					buttonIcons[Yloc][Xloc] = btnType;
+					animate();
+				}	
+			}
+		
+		}
+	}
+	public void animate()
+	{
+		panel.removeAll();
+		for(int i=0;i<Rows;i++)
+		{
+			for(int j = 0; j<Colums; j++)
+			{
+				Buttons[i][j] = new JButton(new ImageIcon(buttonIcons[i][j]));
+				Buttons[i][j].addActionListener(this);
+				Buttons[i][j].setPreferredSize(new Dimension(32, 32));
+			    Buttons[i][j].setMinimumSize(new Dimension(32, 32));
+				Buttons[i][j].setMaximumSize(new Dimension(32, 32));
+				Buttons[i][j].setSize(32, 32);
+				Buttons[i][j].revalidate();
+				panel.add(Buttons[i][j]); 
+			}
+		}
+		window.setContentPane(panel);
+		window.setLayout(new GridLayout(13, 9));
+        window.pack();
+	}
+
+	
 
     public static void main(String[] args)
     {
